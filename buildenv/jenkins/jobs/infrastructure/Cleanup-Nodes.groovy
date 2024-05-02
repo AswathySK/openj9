706b1a109133c4342c5a190f1f450df4eb7d0dc1
Aswathy S Kumar <aswathysk@aswathys-mbp.in.ibm.com>
2024-05-02 14:55:26 +0530

    changes

--- buildenv/jenkins/jobs/infrastructure/Cleanup-Nodes.groovy
+++ buildenv/jenkins/jobs/infrastructure/Cleanup-Nodes.groovy
@@ -185,9 +185,11 @@ timeout(time: TIMEOUT_TIME.toInteger(), unit: TIMEOUT_UNITS) {
                                     // Cleanup zOS datasets 
                                     if (nodeLabels.contains('sw.os.zos')) {
                                         listcat = sh(script: "tso listcat | grep '${env.USER}' | grep 'JVM' | cut -d. -f 2-", returnStdout: true).trim()
-                                        listcat.split('\n').each {
-                                            sh "tso delete ${it}"
+                                        if (listcat != "LISTCAT"){
+                                            listcat.split('\n').each {
+                                                sh "tso delete ${it}"
                                         }
+                                    }
                                     }
 
                                     // Clean up defunct pipelines workspaces
